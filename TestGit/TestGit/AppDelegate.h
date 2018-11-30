86565700f40028cefa57fdeee721b4842ea453b7
EmptyLine <1453715099@qq.com>
2018-11-30 14:08:54 +0800

    添加TestGit工程

---
+++ TestGit/TestGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGit
+//
+//  Created by EmptyLine on 2018/11/30.
+//  Copyright © 2018年 com.emptyline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGit/TestGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGit
+//
+//  Created by EmptyLine on 2018/11/30.
+//  Copyright © 2018年 com.emptyline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
